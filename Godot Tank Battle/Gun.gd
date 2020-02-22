c5e2b7179ac8cf950551618a2ad082d4e6525058
AsgerHB <asgerhb@gmail.com>
2020-02-22 17:07:40 +0100

    Make it so you can only have 4 shots out at a time

--- Godot Tank Battle/Gun.gd
+++ Godot Tank Battle/Gun.gd
@@ -3,16 +3,27 @@ extends Node2D
 export(bool) var is_player2
 export(float) var spawn_distance
 export(float) var bullet_lifetime = 3
+export(int) var max_active_bullets = 4
 export(PackedScene) var Bullet
 
+var active_bullets: int = 0
+
 func shoot():
+	if active_bullets >= max_active_bullets:
+		return
+	
+	active_bullets += 1
 	var b = Bullet.instance()
 	var spawn_rotation = get_parent().rotation + PI
 	var spawn_direction_vector = Vector2(1, 0).rotated(spawn_rotation)
 	var spawn_position = self.get_global_position() + spawn_direction_vector * spawn_distance
 	b.start(spawn_position, spawn_rotation)
 	b.lifetime = bullet_lifetime
+	b.shooter = self
 	get_parent().get_parent().add_child(b)
+
+func register_bullet_freed():
+	active_bullets -= 1
 
 func _process(delta):
 	if is_player2:
